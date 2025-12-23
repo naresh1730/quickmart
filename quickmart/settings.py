f4772b715da92ace050b6439cc34e0fb7411959a
naresh1730 <surepallinaresh1730@gmail.com>
2025-12-23 21:42:08 +0530

    Set DEBUG to False and update STATIC_URL
    Changed DEBUG setting to False for production.

--- quickmart/settings.py
+++ quickmart/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -125,8 +125,8 @@ PAYPAL_MODE = config('PAYPAL_MODE')  # use 'live' in production
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
